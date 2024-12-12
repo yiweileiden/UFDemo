61851a1625ef843cd9d4d7fccf86bea4194b5e02
Yiwei <y.jiang@saxion.nl>
2024-12-12 11:24:33 +0100

    Demo.cs

--- GITDemo/GITDemo/Assets/Demo.cs
+++ GITDemo/GITDemo/Assets/Demo.cs
@@ -7,7 +7,7 @@ public class Demo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int i = 0;
     }
 
     // Update is called once per frame
